7c388cdb10b1b471d2230e9abca096402a068642
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-12-06 11:27:41 +0900

    better code

--- accordination/accordion.js
+++ accordination/accordion.js
@@ -1,22 +1,19 @@
-const section1 = document.querySelector(".section1");
-const section2 = document.querySelector(".section2");
-const section3 = document.querySelector(".section3");
-const hidden = document.querySelector(".hidden");
-const content1 = document.querySelector(".content1");
-const content2 = document.querySelector(".content2");
-const content3 = document.querySelector(".content3");
-console.log(content2);
+var acc = document.getElementsByClassName("accordion");
+var i;
+console.log(acc);
 
-const handleClick2 = () => {
-  content2.classList.toggle("hidden");
-};
-const handleClick1 = () => {
-  content1.classList.toggle("hidden");
-};
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
 
-const handleClick3 = () => {
-  content3.classList.toggle("hidden");
-};
-section1.addEventListener("click", handleClick1);
-section2.addEventListener("click", handleClick2);
-section3.addEventListener("click", handleClick3);
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
