55bbed559af52295f43e03e9717353a787a0bca1
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-12-10 21:52:30 +0900

    learning for

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,22 +1,9 @@
 const tabBar = document.getElementById("tabBar");
-const city = document.getElementsByClassName("city");
+const city = document.querySelectorAll(".city");
 const p = document.querySelectorAll("p");
-console.log(p);
 
-city[0].addEventListener("click", () => {
-  p[0].classList.toggle("l");
-  p[1].classList.add("l");
-  p[2].classList.add("l");
-});
+const handleClick = (event) => {
+  console.log(event.currentTarget);
+};
 
-city[1].addEventListener("click", () => {
-  p[1].classList.toggle("l");
-  p[0].classList.add("l");
-  p[2].classList.add("l");
-});
-
-city[2].addEventListener("click", () => {
-  p[2].classList.toggle("l");
-  p[1].classList.add("l");
-  p[0].classList.add("l");
-});
+city[i].addEventListener("click", handleClick);
