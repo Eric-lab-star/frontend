7a5a5b2e35b216cf2f55ff55c53e0c0ec0c619c6
Eric <cyon256@icloud.com>
2022-02-27 09:48:21 +0900

    delete

--- Menu Icon/icon.js
+++
@@ -1,3 +0,0 @@
-function handleClick(x) {
-  x.classList.toggle("change");
-}
--- tabs/tabs.js
+++
@@ -1,9 +0,0 @@
-const tabBar = document.getElementById("tabBar");
-const city = document.querySelectorAll(".city");
-const p = document.querySelectorAll("p");
-
-const handleClick = (event) => {
-  console.log(event.currentTarget);
-};
-
-city[i].addEventListener("click", handleClick);
